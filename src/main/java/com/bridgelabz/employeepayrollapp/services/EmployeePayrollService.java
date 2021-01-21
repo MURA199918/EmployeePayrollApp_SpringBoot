aae6996aad5993c05514a9a7f8f7292baac7ad7f
MURA199918 <muralisaranr18@gmail.com>
2021-01-21 11:39:44 +0530

    [Murali]Refactored to save employee payroll data to MySQL DB, @entity annotation tells hibernate to create table out of class and created repository interface to hold employee payroll records

--- src/main/java/com/bridgelabz/employeepayrollapp/services/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/services/EmployeePayrollService.java
@@ -3,14 +3,22 @@ package com.bridgelabz.employeepayrollapp.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
 import com.bridgelabz.employeepayrollapp.exceptions.EmployeePayrollException;
 import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
+import com.bridgelabz.employeepayrollapp.repository.EmployeePayrollRepository;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 public class EmployeePayrollService implements IEmployeePayrollService{
+	
+	@Autowired
+	private EmployeePayrollRepository employeeRepository;
 	
 	private List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
 
@@ -30,8 +38,10 @@ public class EmployeePayrollService implements IEmployeePayrollService{
 	public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
 		// TODO Auto-generated method stub
 		EmployeePayrollData empData = null;
-		empData = new EmployeePayrollData(employeePayrollList.size()+1, empPayrollDTO);
-		return empData;
+		empData = new EmployeePayrollData(empPayrollDTO);
+		employeePayrollList.add(empData);
+		log.debug("Emp Data: "+empData.toString());
+		return employeeRepository.save(empData);
 	}
 
 	public EmployeePayrollData updateEmployeePayrollData(int empId,EmployeePayrollDTO empPayrollDTO) {
@@ -39,8 +49,13 @@ public class EmployeePayrollService implements IEmployeePayrollService{
 		EmployeePayrollData empData = this.getEmployeePayrollDataById(empId);
 		empData.setName(empPayrollDTO.name);
 		empData.setSalary(empPayrollDTO.salary);
+		empData.setGender(empPayrollDTO.gender);
+		empData.setStartDate(empPayrollDTO.startDate);
+		empData.setProfilePic(empPayrollDTO.profilePic);
+		empData.setNote(empPayrollDTO.note);
+		empData.setDepartments(empPayrollDTO.departments);
 		employeePayrollList.set(empId-1, empData);
-		return empData;
+		return employeeRepository.save(empData);
 	}
 
 	public void deleteEmployeePayrollData(int empId) {
